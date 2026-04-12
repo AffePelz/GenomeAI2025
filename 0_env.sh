7b170857aeca0417588d02961fb5592a34904dba
Thuan Quang Tran <thuanquangtran.2000@gmail.com>
2026-04-12 18:26:44 +0200

    Fixed bash files
    Fixing files such that the codes could run on Windows

--- 0_env.sh
+++ 0_env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -eu
+sudo apt update
+sudo apt install -y python3-venv python3-pip samtools bedtools
+sudo apt upgrade
+sudo apt autoremove
 
 if [ ! -d ".venv" ]; then
     echo "Creating Python virtual environment..."
@@ -8,7 +12,11 @@ else
     echo "Python virtual environment already exists - skipping creation"
 fi
 
+echo "Activating virtual entironment..."
+. ".venv/bin/activate"
+
 echo "Installing Python dependencies..."
+
 # ----------------------------
 # Install Python packages
 # ----------------------------
--- 1_prepare_dataset.sh
+++ 1_prepare_dataset.sh
@@ -12,44 +12,43 @@ sudo apt install -y python3-venv python3-pip samtools bedtools
 sudo apt upgrade
 sudo apt autoremove
 
-echo "Activating virtual entironment..."
+echo "Activating virtual environment"
 . ".venv/bin/activate"
 
-
-# ----------------------------
+# ---------------------
 # Task 1: Extract chromosome 22 and prepare genome file for bedtools
-# ----------------------------
+# ---------------------
 if [ ! -f "$GENOME_FAI" ]; then
-    echo "Running samtools faidx..."
+    echo "Extracting chromosome 22..."
     samtools faidx "$GENOME" 22 > "$GENOME_CHR22"
 else
-    echo "Chromosome 22 sequence already exists - skipping samtools faidx"
+    echo "Chromosome 22 already extracted."
 fi
 
 awk '$1 == "22"' "$GENOME_FAI" | cut -f1,2 > "$GENOME_FILE"
 
-if [ ! -f "$GENOME_SPLIT" ]; then
+if [ ! -f "$GENOME_FILE" ]; then
     echo "Splitting chromosome 22 into 200bp bins..."
     bedtools makewindows -g "$GENOME_FILE" -w 200 > "$GENOME_SPLIT"
 else
-    echo "Genome already split into bins - skipping bedtools makewindows"
+    echo "Genome bins already created."
 fi
-# ----------------------------
+# ---------------------
 # Task 2: Generate 5-element label vector
-# ----------------------------
+# ---------------------
 echo "Labeling bins with BED files..."
 mkdir -p data/labels
 
 cmds=()
 
 for f in data/bed_data/*.bed; do
-  cmds+=("<(bedtools intersect -a \"$GENOME_SPLIT\" -b \"$f\" -f 0.5 -c | awk '{print (\$NF>0)}')")
+    cmds+=("<(bedtools intersect -a \"$GENOME_SPLIT\" -b \"$f\" -f 0.5 -c | awk '{print (\$NF>0)}')")
 done
 
-# Use eval to expand process substitutions
+# Use eval to expand process substitution
 eval paste "${cmds[@]}" > data/labels/labels_matrix.txt
 
-echo "Labeling complete."
+echo "Labeling completed."
 
 python "main/prepare_dataset.py"
 
